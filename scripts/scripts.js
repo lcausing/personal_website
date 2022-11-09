1bea3a01a06d5185555ba15f6007c280142cd6f4
Louie <causingljbcit@gmail.com>
2022-11-08 19:47:26 -0800

    defined a constant array on scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+const courseList =[
+    {code:"ACIT 1620",name:'Web Fundamental Technology'},
+    {code:"ACIT 1630",name:'Database Systems'},
+    {code:'ACIT 1420', name:'Introduction to Systems Administration'}
+]
